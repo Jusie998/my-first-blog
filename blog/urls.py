233666599147de70b16d1c5822560ad6f70006c8
Jusie998 <jusieaugustine@gmail.com>
2023-03-04 16:44:09 +0800

    Add

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('route2/', views.route_two, name='route_two'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
